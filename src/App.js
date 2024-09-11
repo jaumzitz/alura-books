26c2f315f0080423da4fd0af46932aad00056bd5
joao.souza <joao.fiorini@gmail.com>
2024-09-11 09:34:55 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import Header from './components/Header';
 import Pesquisa from './components/Pesquisa';
+import UltimosLancamentos from './components/UltimosLancamentos';
 import styled from 'styled-components'; //Esse pacote utiliza componentes estilizados para simplificar o uso de CSS nos components.
 
 const AppContainer = styled.div`
 width: 100vw;
 height: 100vh;
-background-image: linear-gradient(90deg, #002F52 35%, #326589);
+background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
 `
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <AppContainer>
       <Header/>
       <Pesquisa/>
+      <UltimosLancamentos/>
     </AppContainer>
   );
 }
--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -8,11 +8,11 @@ import { useState } from 'react';
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 470px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
@@ -34,12 +34,15 @@ const Resultado = styled.div`
     align-items: center;
     margin-bottom: 20px;
     cursor: pointer;
+
     p {
         width: 200px;
     }
+
     img {
         width: 100px;
     }
+
     &:hover {
         border: 1px solid white;
     }
